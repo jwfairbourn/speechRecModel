0a46b7b5a7f37298919d6a34864edc8268864fa8
Jason Fairbourn <jason@jwfdevel.com>
2015-11-02 22:42:45 -0700

    Made separate folder for MP3s and converted everything to WAV; generated a sample .arff file using python; tweaking required

--- arffParser.py
+++ arffParser.py
@@ -12,30 +12,50 @@ def run():
 		files.append(x)
 	females = files[1][2]
 	males = files[2][2]
-	
+
+	output.write("@relation voiceCepstrums\n\n")
+	output.write("@attribute coefficient1 Continuous\n")
+	output.write("@attribute coefficient2 Continuous\n")
+	output.write("@attribute coefficient3 Continuous\n")
+	output.write("@attribute coefficient4 Continuous\n")
+	output.write("@attribute coefficient5 Continuous\n")
+	output.write("@attribute coefficient6 Continuous\n")
+	output.write("@attribute coefficient7 Continuous\n")
+	output.write("@attribute coefficient8 Continuous\n")
+	output.write("@attribute coefficient9 Continuous\n")
+	output.write("@attribute coefficient10 Continuous\n")
+	output.write("@attribute gender {male, female}\n\n")
+
 	output.write("@data\n")
 
 	for filename in males:
-		if filename.endswith(".MP3"):
-			sampFred, data = wavfile.read('AudioRecordings/Male/' + filename);
+		if filename.endswith(".wav") or filename.endswith(".WAV"):
+			sampFreq, data = wavfile.read('AudioRecordings/Male/' + filename)
 			cepstrum = getCepstrum(data)
+			toOutput = []
+			for coefficient in cepstrum:
+				if coefficient[0] != float('Inf') and coefficient[0] != 0.0:
+					toOutput.append(coefficient[0])
 			for i in range(0, 10):
-				output.write(cepstrum[i])
+				output.write(str(toOutput[i]))
 				output.write(", ")
-			output.write("male")
+			output.write("male\n")
 
-	output.write("\n")
 	for filename in females:
-		if filename.endswith(".MP3"):
-			sampFred, data = wavfile.read('AudioRecordings/Female/' + filename);
+		if filename.endswith(".wav") or filename.endswith(".WAV"):
+			sampFreq, data = wavfile.read('AudioRecordings/Female/' + filename)
 			cepstrum = getCepstrum(data)
+			toOutput = []
+			for coefficient in cepstrum:
+				if coefficient[0] != float('Inf') and coefficient[0] != 0.0:
+					toOutput.append(coefficient[0])
 			for i in range(0, 10):
-				output.write(cepstrum[i])
+				output.write(str(toOutput[i]))
 				output.write(", ")
-			output.write("female")
+			output.write("female\n")
 
 def getCepstrum(input):
-    return np.abs(np.fft.ifft((np.log((np.abs(np.fft.fft(input)))*(np.abs(np.fft.fft(input)))))))*np.abs(np.fft.ifft((np.log((np.abs(np.fft.fft(input)))*(np.abs(np.fft.fft(input)))))))
+	return np.abs(np.fft.ifft((np.log((np.abs(np.fft.fft(input)))*(np.abs(np.fft.fft(input)))))))*np.abs(np.fft.ifft((np.log((np.abs(np.fft.fft(input)))*(np.abs(np.fft.fft(input)))))))
 
 if __name__ == "__main__":
     run()
